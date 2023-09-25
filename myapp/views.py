47fc8fae865cf120ea785cd6c8041f6c9d6f6cb7
geetanshpardhi1 <darkwolfcreation7552@gmail.com>
2023-09-25 13:14:45 +0530

    added edit fuctinality to expense

---
+++ myapp/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render,redirect
+from .forms import ExpenseForm
+from .models import Expense
+# Create your views here.
+def index(request):
+    if request.method == "POST":
+        expense = ExpenseForm(request.POST)
+        if expense.is_valid():
+            expense.save()
+    expenses  = Expense.objects.all()
+    expenseform = ExpenseForm()
+    return render(request,'myapp/index.html',{'expenseform':expenseform,'expenses':expenses})
+
+def edit(request,id):
+    expense= Expense.objects.get(id=id)
+    expense_form = ExpenseForm(instance=expense)
+    if request.method=='POST':
+        expense= Expense.objects.get(id=id)
+        form = ExpenseForm(request.POST,instance=expense)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    return render(request,'myapp/edit.html',{'expense_form':expense_form})
